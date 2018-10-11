43ad6b451af9ee2c189f309994af252c3fcf64bb
iHelin <ihelin@outlook.com>
2018-10-11 15:36:48 +0800

    update

--- feign/src/main/java/me/ianhe/cloud/feign/FeignApplication.java
+++ feign/src/main/java/me/ianhe/cloud/feign/FeignApplication.java
@@ -5,12 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/**
+ * @author iHelin
+ */
 @EnableFeignClients
 @EnableEurekaClient
 @SpringBootApplication
-public class ServiceFeignApplication {
+public class FeignApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServiceFeignApplication.class, args);
+        SpringApplication.run(FeignApplication.class, args);
     }
 }
